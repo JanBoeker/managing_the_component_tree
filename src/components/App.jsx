3bdd75af6157146298b72bfc511e8284374afc59
Jan Böker <naj_b@gmx.de>
2021-06-05 20:16:17 +0200

    final commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,6 +17,15 @@ function App() {
     setInputText("");
   }
 
+  function deleteItem(id) {
+    setItems((prevItems) => {
+      return prevItems.filter((item, index) => {
+          return index !== id;
+        }
+      );
+    });
+  }
+
   return (
     <div className="container">
       <div className="heading">
@@ -33,7 +42,9 @@ function App() {
           {items.map((todoItem, index) => (
             <ToDoItem 
               key={index}
+              id={index}
               text={todoItem}
+              onChecked={deleteItem}
             />
           ))}
         </ul>
